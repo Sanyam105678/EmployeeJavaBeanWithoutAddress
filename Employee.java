2224b9355c8a4279d0265df857b642445f2919d0
Sanyam105678 <96380392+Sanyam105678@users.noreply.github.com>
2022-03-22 16:06:32 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import javax.swing.*;
+import java.util.Objects;
+
+public class Employee implements Comparable<Employee>{
+    private int empid;
+    private String name;
+    //private String address;
+    private double salary;
+
+    public Employee(int empid, String name, double salary) {
+        this.empid = empid;
+        this.name = name;
+        //this.address = address;
+        this.salary = salary;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /*public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }*/
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empid=" + empid +
+                ", name='" + name + '\'' +
+
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return empid == employee.empid && Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(empid, name,salary);
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        if(this.salary>o.salary)
+            return -1;
+        else if(this.salary<o.salary)
+            return 1;
+        else{
+            return 0;
+
+
+        }
+
+    }
+}
